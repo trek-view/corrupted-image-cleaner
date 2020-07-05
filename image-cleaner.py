3f5709ea3f61e08f8ab73f9443c3d659d7972359
aa <shineweber00@gmail.com>
2020-07-05 14:46:55 +0300

    - fix

--- image-cleaner.py
+++ image-cleaner.py
@@ -6,44 +6,42 @@
 # Licence: GNU AGPLv3
 # -------------------------------------------------------------------------------
 
-import subprocess
-import shlex
 import os
 import shutil
 import sys
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
+
 
 def checkImage(image_file):
+    proc = Popen(['magick', 'identify', '-verbose', image_file], stdout=PIPE, stderr=STDOUT)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    return exitcode, out, err
 
-          proc = Popen(['magick identify', '-verbose', image_file], stdout=PIPE, stderr=PIPE)
-          out, err = proc.communicate()
-          exitcode = proc.returncode
-          return exitcode, out, err
 
 def main(argv):
-      
-        input_dir = sys.argv[1]
-        output_dir = sys.argv[2]
-        #back_percentage = sys.argv[3] 
-        if not os.path.exists(output_dir):
-               os.makedirs(output_dir)
-        images_files = []
-        # r=root, d=directories, f = files
-        for r, d, f in os.walk(input_dir):
-            for file in f:
-                images_files.append(os.path.join(r, file))
-                             
-        for image_file in images_files:
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    # back_percentage = sys.argv[3]
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    images_files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(os.path.abspath(input_dir)):
+        for file in f:
+            images_files.append(os.path.join(r, file))
 
-              code, output, error = checkImage(image_file)
-              if str(code) !="0" or str(error, "utf-8") != "":
-                print("ERROR " + image_file)
-              else:
-                print("OK " + image_file)
-                shutil.copy2(image_file, output_dir)
-  
-print('staring...')  
+    for image_file in images_files:
+
+        code, output, error = checkImage(image_file)
+        if str(code) != "0" or (error and str(error, "utf-8") != ""):
+            print("ERROR " + image_file)
+        else:
+            print("OK " + image_file)
+            shutil.copy2(image_file, output_dir)
+
+
+print('staring...')
 
 if __name__ == '__main__':
-    
-     main(sys.argv[1:])    
+    main(sys.argv[1:])
